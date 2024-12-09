49f7e0cc98704d5cf5ee9edb2ad2bc1f57c04e37
Kumar Shivam <thecode.shivam@gmail.com>
2024-12-10 01:43:00 +0530

    git remote

--- MyJdbcproj/src/myjdbc/MyJdbct.java
+++ MyJdbcproj/src/myjdbc/MyJdbct.java
@@ -7,7 +7,9 @@ public class MyJdbct {
 		
    Students st = new Students();
 //   st.createDatabase();
-   st.createTable();
+//   st.createTable();
+//   st.createData();
+   st.readData();
      
 	}
 
--- MyJdbcproj/src/myjdbc/Students.java
+++ MyJdbcproj/src/myjdbc/Students.java
@@ -3,6 +3,8 @@ package myjdbc;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.Statement;
 
 public class Students {
@@ -53,4 +55,66 @@ public class Students {
 		
 		
 	}
+
+	public void createData() {
+		// TODO Auto-generated method stub
+		try {
+        	// Database credentials
+        	String jdbcURL = "jdbc:mysql://localhost:3306/shivamtest"; 
+        	String username = "root"; 
+        	String password = "Shivam@995579"; 
+            // Establishing connection
+            Connection conn = DriverManager.getConnection(jdbcURL, username, password);
+
+            String sql = "INSERT INTO student (sid,sname,semail) VALUES (?,?,?)";
+            PreparedStatement pstm = conn.prepareStatement(sql);
+           
+           pstm.setInt(1,10);
+           pstm.setString(2,"shivam");
+           pstm.setString(3, "shivam@gmail.com");
+           
+     
+            pstm.execute();
+            System.out.println("Data  inserted Successfully");
+
+            conn.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+	
+	
 }
+
+	public void readData() {
+		// TODO Auto-generated method stub
+		try {
+        	// Database credentials
+        	String jdbcURL = "jdbc:mysql://localhost:3306/shivamtest"; 
+        	String username = "root"; 
+        	String password = "Shivam@995579"; 
+            // Establishing connection
+            Connection conn = DriverManager.getConnection(jdbcURL, username, password);
+
+            String sql = "Select * from student";
+            Statement stm = conn.createStatement();
+            ResultSet rs = stm.executeQuery(sql);
+           
+           while(rs.next()) {
+        	   System.out.println("id ="+ rs.getInt(1));
+        	   System.out.println("id ="+ rs.getString(2));
+        	   System.out.println("id ="+ rs.getString(3));
+           }
+     
+            
+            System.out.println("read Successfully");
+
+            conn.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+		
+	}
+		
+		
+	}
+
